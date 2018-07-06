c8af6f6dcced3e3a931973944c3058013d6587c9
wanzzhehe <wanzzemail@gmail.com>
2018-07-07 00:13:09 +0800

    Untracked files

---
+++ 001.Monty Hall problem/main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+class Random{ 
+#ifdef __linux
+	private:
+		T res;
+		FILE *f;
+	public:
+		Random() { 
+			f = fopen("/dev/random", "r");
+		}
+		~Random() { 
+			fclose(f);
+		}
+		T operator () (void) { 
+			fread(&res, sizeof(T), 1, f);
+			return res;
+		}
+#endif
+#ifdef _WIN32
+	public:
+		Random() { 
+			srand(time(NULL));
+		}
+		T operator () (void) { 
+			return rand();
+		}
+#endif
+};
+
+Random<unsigned> a;
+unsigned long long Casenum;
+int WhichDoor, FirstSelect;
+int SecontSelect;
+unsigned long long cnt0, cnt1, cnt;
+bool Selected[3];
+
+int main() { 
+	printf("Please enter the number of repetitions: ");
+	scanf("%llu", &Casenum);
+	cnt = Casenum;
+	while(Casenum--) { 
+		memset(Selected, 0x00, sizeof(Selected));
+		WhichDoor = a() % 3;
+		FirstSelect = a() % 3;
+		Selected[FirstSelect] = true;
+		do{ 
+			SecontSelect = a() % 3;
+		}while(Selected[SecontSelect] || SecontSelect == WhichDoor);
+		Selected[SecontSelect] = true;
+		for(int i = 0; i != 3; ++i)
+			if(!Selected[i] && i == WhichDoor) { 
+				++cnt0;
+				goto END;
+			}
+END:	continue;
+	}
+	printf("Total : %llu\n", cnt);
+	printf("You win : %llu\n", cnt0);
+	printf("You lose: %llu\n", cnt - cnt0);
+	return 0;
+}
